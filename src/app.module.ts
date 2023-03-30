db5641f9f6ffc13d792c18fd11ce9c363c7a8c51
Andrew M. Silva <andrewsaxx@gmail.com>
2023-03-30 17:03:48 -0300

    adjusting imports and modules, and connecting to mongo

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,15 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { UserModule } from './modules/user.module';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost:27017/HotelAdmin')],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.DATABASE_URI, {
+      dbName: process.env.DATABASE_NAME,
+    }),
+    UserModule,
+  ],
 })
 export class AppModule {}
