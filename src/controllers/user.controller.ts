867e6c9f336887fbfca40315597389abeb501063
Andrew M. Silva <andrewsaxx@gmail.com>
2023-03-30 18:05:48 -0300

    creating controller

---
+++ src/controllers/user.controller.ts
@@ -0,0 +1,14 @@
+import { Controller, Post, Body } from '@nestjs/common';
+import { User } from 'src/entities/user.entity';
+import { CreateUserDTO } from 'src/usecases/create-user/create-user.dto';
+import { CreateUserUseCase } from 'src/usecases/create-user/create-user.usecase';
+
+@Controller('user')
+export class UserController {
+  constructor(private readonly createUserUseCase: CreateUserUseCase) {}
+
+  @Post()
+  createUser(@Body() userProps: CreateUserDTO): Promise<User> {
+    return this.createUserUseCase.execute(userProps);
+  }
+}
