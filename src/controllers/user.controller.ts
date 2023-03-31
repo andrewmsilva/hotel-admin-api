f18100c4bf7368a4c77ed4e230c74c4ccb8840fd
Andrew M. Silva <andrewsaxx@gmail.com>
2023-03-31 10:13:50 -0300

    creating e2e tests

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, HttpCode } from '@nestjs/common';
 import { User } from 'src/entities/user.entity';
 import { CreateUserDTO } from 'src/usecases/create-user/create-user.dto';
 import { CreateUserUseCase } from 'src/usecases/create-user/create-user.usecase';
@@ -8,6 +8,7 @@ export class UserController {
   constructor(private readonly createUserUseCase: CreateUserUseCase) {}
 
   @Post()
+  @HttpCode(201)
   createUser(@Body() userProps: CreateUserDTO): Promise<User> {
     return this.createUserUseCase.execute(userProps);
   }
