5b57fb737fbe67dbb4543c4b14ab149626be12e5
Andrew M. Silva <andrewsaxx@gmail.com>
2023-04-03 15:37:22 -0300

    adjusting modules

--- src/modules/hotel.module.ts
+++ src/modules/hotel.module.ts
@@ -2,17 +2,17 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { HotelRepository } from 'src/repositories/hotel/hotel.repository';
 import { HotelModel, HotelSchema } from 'src/repositories/hotel/hotel.schema';
+import { RoomRepository } from 'src/repositories/room/room.repository';
+import { RoomModel, RoomSchema } from 'src/repositories/room/room.schema';
 
 @Module({
   imports: [
     MongooseModule.forFeature([
-      {
-        name: HotelModel.name,
-        schema: HotelSchema,
-      },
+      { name: HotelModel.name, schema: HotelSchema },
+      { name: RoomModel.name, schema: RoomSchema },
     ]),
   ],
   controllers: [],
-  providers: [HotelRepository],
+  providers: [HotelRepository, RoomRepository],
 })
 export class HotelModule {}
