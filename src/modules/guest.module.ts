c0674472edc257721282b368dd9489fd08596fcc
Andrew M. Silva <andrewsaxx@gmail.com>
2023-04-03 11:36:30 -0300

    adding guest repo to module

--- src/modules/guest.module.ts
+++ src/modules/guest.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { GuestRepository } from 'src/repositories/guest/guest.repository';
 import { GuestModel, GuestSchema } from 'src/repositories/guest/guest.schema';
 
 @Module({
@@ -12,6 +13,6 @@ import { GuestModel, GuestSchema } from 'src/repositories/guest/guest.schema';
     ]),
   ],
   controllers: [],
-  providers: [],
+  providers: [GuestRepository],
 })
 export class GuestModule {}
