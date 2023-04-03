7a72e6c6a6bc94b91ba72244f702ead504a83ec2
Andrew M. Silva <andrewsaxx@gmail.com>
2023-04-03 15:42:39 -0300

    creating entity and schema

---
+++ src/entities/booking.entity.ts
@@ -0,0 +1,21 @@
+import { Guest } from './guest.entity';
+import { Room } from './room.entity';
+
+export class Booking {
+  readonly id: string;
+
+  guest: Guest;
+  room: Room;
+  startAt: Date;
+  endAt: Date;
+
+  constructor(props: Booking) {
+    this.id = props.id;
+    this.guest = props.guest;
+    this.room = props.room;
+    this.startAt = props.startAt;
+    this.endAt = props.endAt;
+  }
+}
+
+export type BookingProps = Omit<Booking, 'id'>;
