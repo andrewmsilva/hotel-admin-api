f983c47d8c0ab2917149291f028a0af413a94c42
Andrew M. Silva <andrewsaxx@gmail.com>
2023-04-06 14:49:55 -0300

    avoiding seed user

--- src/jobs/seed.job.ts
+++ src/jobs/seed.job.ts
@@ -5,18 +5,12 @@ import { HotelRepository } from 'src/repositories/hotel/hotel.repository';
 import { HotelModel } from 'src/repositories/hotel/hotel.schema';
 import { RoomRepository } from 'src/repositories/room/room.repository';
 import { RoomModel } from 'src/repositories/room/room.schema';
-import { UserRepository } from 'src/repositories/user/user.repository';
-import { UserModel } from 'src/repositories/user/user.schema';
 import { Seed } from 'src/seeds/seed';
 
 async function bootstrap() {
   const app = await NestFactory.createApplicationContext(AppModule);
 
   const seed = new Seed();
-
-  const userRepository = app.get(UserRepository);
-  const userModel: Model<UserModel> = (userRepository as any).userModel;
-  await userModel.create(seed.user.createProps());
 
   const hotelRepository = app.get(HotelRepository);
   const hotelModel: Model<HotelModel> = (hotelRepository as any).hotelModel;
