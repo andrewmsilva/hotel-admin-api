d9e4e5093e3db3460861c1dfe7449d5e37cb675d
Andrew M. Silva <andrewsaxx@gmail.com>
2023-04-06 19:19:29 -0300

    creating usecase

---
+++ src/usecases/booking/get-booking/get-booking.usecase.ts
@@ -0,0 +1,25 @@
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { Booking } from 'src/entities/booking.entity';
+import { BookingRepository } from 'src/repositories/booking/booking.repository';
+import { GetBookingDTO } from './get-booking.dto';
+
+@Injectable()
+export class GetBookingUseCase {
+  constructor(private readonly bookingRepository: BookingRepository) {}
+
+  async execute(
+    { bookingId }: GetBookingDTO,
+    userId: string,
+  ): Promise<Booking> {
+    const booking = await this.bookingRepository.findOneByIdAndUser(
+      bookingId,
+      userId,
+    );
+
+    if (!booking) {
+      throw new HttpException('Booking not found', HttpStatus.NOT_FOUND);
+    }
+
+    return booking;
+  }
+}
